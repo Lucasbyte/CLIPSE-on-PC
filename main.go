d4f231880c224901bfc1d3fee6d3dd10d91f7a38
Lucas Isabel <lucasestudo2020@gmail.com>
2024-03-19 09:43:51 -0300

    Readme

--- main.go
+++ main.go
@@ -1,12 +1,14 @@
 package main
 
 import (
+	"fmt"
 	"net/http"
 
 	"github.com/lucasbyte/go-clipse/routes"
 )
 
 func main() {
+	fmt.Println()
 	routes.CarregaRotas()
 	http.ListenAndServe(":8000", nil)
 }
---
+++ readme.md
@@ -0,0 +1,1 @@
+# CLIPSE-PC
